07effa57395dfa3bae661fabec70456f649604d8
Ashish Patel <ashishpatelmedia@icloud.com>
2017-03-27 16:35:25 +0000

    mockup generator app

---
+++ index.php
@@ -0,0 +1,101 @@
+<!--
+Author: Ashish Patel
+Full Stack Web Developer & MCA Student
+National Institute of Technology, Agartala
+
+Mockup Generator app for software developer challenge as aspiring intern at AppLaunchpad.
+
+Template from my github account - https://github.com/ashishpatelcs/boilerplate
+-->
+
+<!doctype html>
+
+<!-- HTML tag -->
+<html lang="en">
+
+    <!-- Let's get started -->
+    <head>
+
+        <!-- Document settings and metadata -->
+        <title>Mockup Generator - Software Developer Challenge @ AppLaunchpad</title>
+        <meta charset="UTF-8" />
+        <meta name="description" content="Mockup Generator app for software developer challenge as aspiring intern at AppLaunchpad." />
+        <meta name="author" content="Ashish Patel" />
+        <meta name="designer" content="Ashish Patel" />
+        <meta name="rating" content="" />
+        <meta name="keywords" content="applaunchpad, software developer challenge, mockup generator" />
+
+        <!-- iOS, Android and Windows stuff -->
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <meta name="format-detection" content="telephone=yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileImage" content="" />
+        <meta name="msapplication-TileColor" content="" />
+
+        <!-- Robots and Viewport -->
+        <meta name="robots" content="index, follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+        <!-- OpenGraph for Facebook -->
+        <meta property="og:type" content="" />
+        <meta property="og:title" content="" />
+        <meta property="og:url" content="" />
+        <meta property="og:description" content="" />
+        <meta property="og:image" content="" />
+        <meta property="og:locale" content="" />
+
+        <!-- Twitter Card -->
+        <meta property="twitter:card" content="" />
+        <meta property="twitter:site" content="" />
+        <meta property="twitter:title" content="" />
+        <meta property="twitter:description" content="" />
+        <meta property="twitter:image" content="" />
+
+        <!-- Icons -->
+        <link rel="icon" href="" />
+        <link rel="icon" sizes="196x196" href="" />
+        <link rel="apple-touch-icon" href="" />
+
+        <!-- Prerendering and Prefetching -->
+        <link rel="prerender" href="" />
+        <link rel="prefetch" href="" />
+        <link rel="dns-prefetch" href="" />
+
+        <!-- Miscellaneous and SEO stuff -->
+        <link rel="author" href="humans.txt" />
+        <link rel="canonical" href="" />
+        <link rel="sitemap" href="" />
+        <link rel="next" href="" />
+        <link rel="prev" href="" />
+
+        <!-- Stylesheets go here (all the css and js files can be minified and compressed in actual production server :) -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css" />
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/basics/1.2.2/basics.css" />
+
+        <!-- Header scripts go here -->
+        <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js"></script>
+
+    </head>
+    <body>
+        <div class="container">
+            <h1>Welcome</h1>
+        </div>
+
+        <!-- Footer scripts go here -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/retina.js/1.3.0/retina.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js"></script>
+        <script>
+            if ('addEventListener' in document) {
+                document.addEventListener('DOMContentLoaded', function() {
+                    FastClick.attach(document.body);
+                }, false);
+            }
+        </script>
+
+    </body>
+</html>
